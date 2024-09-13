8dcb107fd6133e7c5a96cf6ff326330a8b1ddecb
Josiah McCracken <scribhneoir21@gmail.com>
2024-09-13 14:09:53 -0400

    fix: carry issue

--- index.ts
+++ index.ts
@@ -11,24 +11,40 @@ const mapToAlphabet = (num: string) => {
   return result;
 };
 
-const handleZeros = (base26: string[]) => {
+const handleZ = (base26: string[]): string[] => {
+  if (base26.length === 1) {
+    if (base26[0] === "A") {
+      base26[0] = "";
+    } else {
+      const previousLetterAsInt = parseInt(base26[0], 36) - 10;
+      base26[0] = mapToAlphabet(previousLetterAsInt.toString(26));
+    }
+    return base26;
+  } else {
+    if (base26[base26.length - 1] === "A") {
+      return [...handleZ(base26.slice(0, base26.length - 1)), "Z"];
+    } else {
+      const previousLetterAsInt = parseInt(base26[base26.length - 1], 36) - 10;
+      return [
+        ...base26.slice(0, base26.length - 1),
+        mapToAlphabet(previousLetterAsInt.toString(26)),
+      ];
+    }
+  }
+};
+
+const base26ToAlpha = (base26: string[]) => {
   for (let i = 0; i < base26.length; i++) {
     const letter = mapToAlphabet(base26[i]);
     base26[i] = letter;
     if (letter === "Z") {
-      const previousLetterAsInt = parseInt(base26[i - 1], 36) - 10;
-      if (previousLetterAsInt === 0) {
-        base26[i - 1] = "";
-      } else {
-        const alphabet = "ZABCDEFGHIJKLMNOPQRSTUVWXY";
-        base26[i - 1] = alphabet[previousLetterAsInt];
-      }
+      base26 = [...handleZ(base26.slice(0, i)), "Z", ...base26.slice(i + 1)];
     }
   }
   return base26.join("");
 };
 
-const numberToAlphabet = (num: number) => {
+const toAlphaBase = (num: number) => {
   if (num < 1) {
     return {
       success: false,
@@ -37,14 +53,16 @@ const numberToAlphabet = (num: number) => {
   }
 
   const base26 = decimalToBase(num, 26);
-  const result = handleZeros(base26.split(""));
+  const result = base26ToAlpha(base26.split(""));
 
   return result;
 };
 
 let data = "";
 for (let i = 1; i <= 13520; i++) {
-  data += `${numberToAlphabet(i)}\n`;
+  data += `${toAlphaBase(i)}\n`;
 }
 
 await Bun.write("output.txt", data);
+
+console.log(toAlphaBase(702)); // "ZZZ"
